39b0a8c63d75f8a91c42df661ff36145b05f1f55
Marietta Pruuli <marietta.pruuli@testreel.com>
2019-03-21 12:50:42 +0200

    WIP try to show current floors while moving

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,10 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import sample.datamodel.Building;
 import sample.datamodel.ControlSystem;
@@ -16,6 +18,8 @@ public class Controller {
     private TextField targetFloor;
     @FXML
     private Button callButton;
+    @FXML
+    private Label el1, el2, el3;
 
     private static Building building = new Building();
     private static ControlSystem controlSystem = building.getControlSystem();
@@ -30,6 +34,31 @@ public class Controller {
             Elevator current = building.getElevators().get(i);
             current.getThread().start();
         }
+
+        System.out.println(building.getElevators().get(0).getThread());
+        System.out.println(building.getElevators().get(1).getThread());
+        System.out.println(building.getElevators().get(2).getThread());
+
+        System.out.println("El 1 " + building.getElevators().get(0).getCurrentFloor());
+        System.out.println("El 2 " + building.getElevators().get(1).getCurrentFloor());
+        System.out.println("El 3 " + building.getElevators().get(2).getCurrentFloor());
+
+    }
+
+    @FXML
+    public void watchFloors() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                String currentfloor1 = ""+ building.getElevators().get(0).getCurrentFloor();
+                String currentfloor2 = ""+ building.getElevators().get(1).getCurrentFloor();
+                String currentfloor3 = ""+ building.getElevators().get(2).getCurrentFloor();
+                el1.setText(currentfloor1);
+                el2.setText(currentfloor2);
+                el3.setText(currentfloor3);
+
+            }
+        });
     }
 
     @FXML
@@ -63,5 +92,17 @@ public class Controller {
         if (!controlSystem.addFloorCall(newCall)) {
             System.out.println("Could not add floor request to call stack");
         }
+    }
+
+    public Label getEl1() {
+        return el1;
+    }
+
+    public Label getEl2() {
+        return el2;
+    }
+
+    public Label getEl3() {
+        return el3;
     }
 }
